66586c6f0d0aca596d8301a093190cd18f36c436
Whitedaddy <31011162+Whitedaddy@users.noreply.github.com>
2021-12-06 22:44:01 +0300

    Initial Commit

---
+++ Extas/ExtasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtasApp.swift
+//  Extas
+//
+//  Created by MacBook on 06.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ExtasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExtasTests/ExtasTests.swift
@@ -0,0 +1,33 @@
+//
+//  ExtasTests.swift
+//  ExtasTests
+//
+//  Created by MacBook on 06.12.2021.
+//
+
+import XCTest
+@testable import Extas
+
+class ExtasTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
